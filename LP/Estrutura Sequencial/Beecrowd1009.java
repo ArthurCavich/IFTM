69e99dce449e02207832c265a24a0c8e9f1cb5bf
Arthur Santana Cavichioli <104861207+Arthurcavich@users.noreply.github.com>
2025-04-02 21:45:58 -0300

    Salário com Bônus

---
+++ LP/Estrutura Sequencial/Beecrowd1009.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        
+        Scanner s = new Scanner(System.in);
+        
+        String nome = s.nextLine();
+        double salario = s.nextDouble();
+        double vendas = s.nextDouble();
+        double comissao = vendas * 0.15;
+        double salarioFinal = salario + comissao;
+
+        System.out.printf("TOTAL = R$ %.2f%n", salarioFinal);
+
+    }
+ 
+}
