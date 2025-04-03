25de03c0bcc8cf52ec884e27aa98b3cc53d72edc
Arthur Santana Cavichioli <104861207+Arthurcavich@users.noreply.github.com>
2025-04-02 21:41:38 -0300

    Média 1

---
+++ LP/Estrutura Sequencial/Beecrowd1005.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        
+        Scanner s = new Scanner(System.in);
+        
+        double A = s.nextDouble();
+        double B = s.nextDouble();
+        double MEDIA = ((A * 3.5) + (B * 7.5)) / 11.0;
+        System.out.printf("MEDIA = %.5f%n", MEDIA);
+
+    }
+ 
+}
