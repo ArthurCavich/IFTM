a5f1e0298ed2239c16095bb6c7c973ec4227a3b8
Arthur Santana Cavichioli <104861207+Arthurcavich@users.noreply.github.com>
2025-04-02 22:10:45 -0300

    Consumo

--- LP/Estrutura Sequencial/Beecrowd1014.java
+++ LP/Estrutura Sequencial/Beecrowd1014.java
@@ -1,1 +1,19 @@
+import java.io.IOException;
+import java.util.Scanner;
 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        
+        Scanner s = new Scanner(System.in);
+        
+        double x = s.nextDouble();
+        double y = s.nextDouble();
+        
+        double consumo = x / y;
+        
+        System.out.printf("%.3f km/l\n", consumo);
+
+    }
+ 
+}
