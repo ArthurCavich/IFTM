342776cdef04ed25879aaccfb7d6e1239d3f794a
Arthur Santana Cavichioli <arthurcavic@gmail.com>
2025-06-15 20:12:02 -0300

    Update Beecrowd1170.java

--- LP/Vetores/src/Beecrowd1170.java
+++ LP/Vetores/src/Beecrowd1170.java
@@ -3,19 +3,15 @@ public class Beecrowd1170 {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
 
+        int n = sc.nextInt();
         
-        int N = sc.nextInt();
+        double[] comida = new double[n];
 
-        
-        double[] comida = new double[N];
-
-        
         for (int i = 0; i < n; i++) {
             comida[i] = sc.nextDouble();
         }
-
        
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < n; i++) {
             int dias = 0;
             while (comida[i] > 1.0) {
                 comida[i] /= 2.0;
