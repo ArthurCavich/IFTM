177f0cbcbd8a764d6e7088b589ed7ca112505c83
Arthur Santana Cavichioli <arthurcavic@gmail.com>
2025-09-21 20:44:07 -0300

    Inserido atividade da matéria MVC utilizando SpringBoot

---
+++ 2Period/MVC/Exercício 1/mvc-thymeleaf-demo/src/test/java/br/iftm/edu/mvc_thymeleaf_demo/MvcThymeleafDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.iftm.edu.mvc_thymeleaf_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcThymeleafDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
