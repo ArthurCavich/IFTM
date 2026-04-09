a37735a83e54d1611abc22cc1c63426a3f1dffee
Arthur Santana Cavichioli <arthurcavic@gmail.com>
2026-04-09 16:44:47 -0300

    Aula sobre JUNIT na camada Repository.
    Aula ensinando sobre como realizar testes na cama repository, onde ficam as comunicações com o banco de dados.

---
+++ 3Period/Automated Tests/projeto-veterinario-springboot-h2-master/gerenciador-Veterinarios/src/test/java/com/cod3r/gerenciadorfuncionarios/repositories/VeterinarioRepositoryTest.java
@@ -0,0 +1,67 @@
+package com.cod3r.gerenciadorfuncionarios.repositories;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.iftm.gerenciadorveterinarios.entities.Veterinario;
+import org.iftm.gerenciadorveterinarios.repositories.VeterinarioRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class) // define a forma de ordenar os métodos
+public class VeterinarioRepositoryTest {
+    @Autowired // faz injeção de dependência da classe a ser testada
+    private VeterinarioRepository repositorio;
+
+    @Test
+    void test() {
+        fail("not yet implemented");
+    }
+
+    @Test
+    @DisplayName("Testa se a busca pelo ID realmente retorna o veterinario correto.")
+    @Order(1) // Definir ordem do caso de teste
+    public void testarBuscaPorIdRetornaVeterinarioCorreto1() {
+        // arrange
+        Integer idexistente = 1;
+        String nomeEsperado = "Conceição Evaristo";
+        String emailEsperado = "conceicao@gmail.com";
+
+        // Act
+        Veterinario veterinarioObtido = repositorio.getById(idexistente);
+
+        // Assert
+        assertNotNull(veterinarioObtido);
+        assertEquals(nomeEsperado, veterinarioObtido.getNome());
+        assertEquals(emailEsperado, veterinarioObtido.getEmail());
+    }
+
+    @Test
+    @DisplayName("Teste realizado igual o de cima porém utilizando o Optional dentro da lista de objetos")
+    @Order(2)
+    public void testarBuscaPorIdRetornaVeterinarioCorretoComOptional() {
+        // arrange
+        Integer idExistente = 1;
+        String nomeEsperado = "Conceição Evaristo";
+        String emailEsperado = "conceicao@gmail.com";
+
+        // Act
+        Optional<Veterinario> veterinarioEncontrado = repositorio.findById(idExistente);
+
+        // Assert
+        assertNotNull(veterinarioEncontrado);
+        assertEquals(nomeEsperado, veterinarioEncontrado.get().getNome());
+        assertEquals(emailEsperado, veterinarioEncontrado.get().getEmail());
+    }
+
+}
