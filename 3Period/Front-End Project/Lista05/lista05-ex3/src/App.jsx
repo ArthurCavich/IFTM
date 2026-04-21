569a990dfed3d3cd4912f30a6e80febacb162944
Arthur Santana Cavichioli <arthurcavic@gmail.com>
2026-04-21 20:43:01 -0300

    Término da atividade de React - introdução à biblioteca
    Enviando o segundo projeto React com a build para envio ao professor.

---
+++ 3Period/Front-End Project/Lista05/lista05-ex3/src/App.jsx
@@ -0,0 +1,19 @@
+import Card from './components/Card';
+
+function App() {
+
+  return (
+    <div className="container">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ 3Period/Front-End Project/Lista05/lista05-ex3/src/components/Card.jsx
@@ -0,0 +1,8 @@
+export default function Card() {
+    return (
+        <div className="card">
+            <h2>Título da Página</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat voluptates nesciunt totam illum voluptate, corrupti perspiciatis voluptatum deserunt minima nostrum officia autem? Repudiandae est cumque porro culpa praesentium blanditiis obcaecati?</p>
+        </div>
+    );
+}
