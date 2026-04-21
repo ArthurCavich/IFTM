411d5762c9b574d0f3a4b8f6fcdef5383d03eddb
Arthur Santana Cavichioli <arthurcavic@gmail.com>
2026-04-21 19:36:27 -0300

    Atividade de React.
    Atividade consiste em criar componentes para criação de uma seção da página. Utilizei 3 componentes e fiz o css de cada. Ao final, fiz um build para hospedar e enviar ao professor.

--- 3Period/Front-End Project/react-basic/app01/src/App.jsx
+++ 3Period/Front-End Project/react-basic/app01/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
             <Card />
             <Card />
         </div>
+        
     );
 }
 
--- 3Period/Front-End Project/react-basic/app01/src/Card.jsx
+++ 3Period/Front-End Project/react-basic/app01/src/Card.jsx
@@ -2,7 +2,7 @@ import React, { } from 'react';
 
 function Card() {
     return (
-        <div>
+        <div className='card'>
             <h2>Título do Card</h2>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae explicabo nemo voluptate deserunt quisquam repudiandae accusamus facilis accusantium soluta iste corporis maiores, doloremque quibusdam placeat necessitatibus minima earum pariatur corrupti?</p>
         </div>
