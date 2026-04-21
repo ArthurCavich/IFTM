5499f61f54735ea6a328b591d414ed740b6c4a76
Arthur Santana Cavichioli <arthurcavic@gmail.com>
2026-04-21 20:10:06 -0300

    Correção das partes 1 e 2 do exercício.

--- 3Period/Front-End Project/Lista05/Lista05/src/App.jsx
+++ 3Period/Front-End Project/Lista05/Lista05/src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css';
 import Cabecalho from './components/Cabecalho';
 import Card from './components/Card';
-import Botao from './components/Botao';
 
 export function App() {
   return (
@@ -13,17 +12,6 @@ export function App() {
       
       <div className="cards-grid">
         <Card titulo="PLANO DE DESENVOLVIMENTO INSTITUCIONAL" />
-        <Card titulo="PESQUISA PÚBLICA PROCESSOS IFTM" />
-        <Card titulo="LICITAÇÕES E CONTRATOS" />
-        <Card titulo="RECEITAS E DESPESAS" />
-        <Card titulo="DADOS ABERTOS" />
-        <Card titulo="TRANSPARÊNCIA E PRESTAÇÃO DE CONTAS" />
-        <Card titulo="SERVIDORES" />
-        <Card titulo="PERGUNTAS FREQUENTES" />
-      </div>
-
-      <div className="button-container">
-        <Botao texto="Mais informações"/>
       </div>
     </div>
   );
--- 3Period/Front-End Project/Lista05/Lista05/src/components/Botao.jsx
+++
@@ -1,9 +0,0 @@
-import './Botao.css';
-
-export default function Botao({ texto, onClick }) {
-    return (
-        <button className="btn" onClick={onClick}>
-            {texto}
-        </button>
-    );
-}
