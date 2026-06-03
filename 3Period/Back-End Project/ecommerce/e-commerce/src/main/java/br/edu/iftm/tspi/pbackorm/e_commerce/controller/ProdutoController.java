df9e52f23f7215f1f89819f861bbcc999d5de0e5
Arthur Santana Cavichioli <arthurcavic@gmail.com>
2026-06-02 22:19:37 -0300

    Aula de Back-end

--- 3Period/Back-End Project/ecommerce/e-commerce/src/main/java/br/edu/iftm/tspi/pbackorm/e_commerce/controller/ProdutoController.java
+++ 3Period/Back-End Project/ecommerce/e-commerce/src/main/java/br/edu/iftm/tspi/pbackorm/e_commerce/controller/ProdutoController.java
@@ -33,5 +33,7 @@ public class ProdutoController {
                         .status(HttpStatus.CREATED)
                         .body(produtoSalvoDto);
     }
+
+    
     
 }
--- 3Period/Back-End Project/ecommerce/e-commerce/src/main/java/br/edu/iftm/tspi/pbackorm/e_commerce/domain/Pedido.java
+++ 3Period/Back-End Project/ecommerce/e-commerce/src/main/java/br/edu/iftm/tspi/pbackorm/e_commerce/domain/Pedido.java
@@ -38,6 +38,8 @@ public class Pedido {
     @OneToMany(mappedBy="pedido",fetch=FetchType.EAGER,cascade=CascadeType.ALL)
     private List<DetalhePedido> detalhesPedido;
     
-
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name = "categoriaID", nullable=false)
+    private Categoria categoria;
 
 }
